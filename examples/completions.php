910710c2283cc4f33b16385ba154de8ba65d3426
chipslays <gorshkov34@gmail.com>
2026-06-29 20:39:40 +0400

    update example

--- examples/completions.php
+++ examples/completions.php
@@ -4,12 +4,11 @@ use Ollama\Ollama;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$ollama = new Ollama();
+$ollama = new Ollama;
 
 $response = $ollama->completions()->generate([
     'model' => 'ornith:9b-q4_K_M',
     'prompt' => 'Why is the sky blue?',
-    'thinking' => false,
 ]);
 
 $response->dd();
